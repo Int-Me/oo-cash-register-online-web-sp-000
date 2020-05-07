7e7d67a8292551036d66fcbbce7a187e1c845918
Roland McRae <mcraeroland@gmail.com>
2020-05-07 10:57:38 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :total, :discount, :price, :items
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_items(item, price, amount = 1)
+    @price = price
+    
+  end
+
+
+end
